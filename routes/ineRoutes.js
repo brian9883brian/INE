4d1bef37e8a339bc5c53c5434a05b5ae65db3dfb
brian9883brian <142156650+brian9883brian@users.noreply.github.com>
2025-05-20 19:08:14 -0600

    Update ineRoutes.js

--- routes/ineRoutes.js
+++ routes/ineRoutes.js
@@ -7,6 +7,7 @@ router.post('/', ineController.create);
 router.get('/', ineController.getAll);
 router.get('/:id', ineController.getById);
 router.get('/curp/:curp', ineController.getByCurp);
+router.get('/clave/:clave_ine', ineController.getByClaveIne); // Nueva ruta
 router.delete('/curp/:curp', ineController.deleteByCurp);
 
 module.exports = router;
